2a037f2345f2c553d494a93dd8a7d9910e5571c6
1vket <ivket3@gmail.com>
2022-04-05 17:40:56 +0900

    first commit

---
+++ AttentionASR/src/model.py
@@ -0,0 +1,116 @@
+import math
+import logging
+import defect
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+logger = logging.getLogger(__name__)
+
+class TransformerModel(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+
+    self.e_pos_emb = nn.Parameter(
+      torch.zeros(1, config.block_size, config.n_mel))
+    self.ln = nn.LayerNorm(config.n_mel)
+    self.dense = nn.Linear(config.n_mel, config.d_model)
+
+    self.tok_emb = nn.Embedding(config.vocab_size, config.d_model)
+    self.d_pos_emb = nn.Parameter(
+      torch.zeros(1, config.block_size, config.d_model))
+
+    self.transformer = nn.Transformer(
+      d_model = config.d_model,
+      num_encoder_layers = config.n_layer,
+      num_decoder_layers = config.n_layer,
+      dim_feedforward = config.d_model * 4,
+      activation = 'gelu',
+      batch_first=True)
+
+    self.out = nn.Linear(config.d_model, config.vocab_size)
+
+    self.apply(self._init_weights)
+
+  def _init_weights(self, module):
+    if isinstance(module, (nn.Linear, nn.Embedding)):
+      torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+      if isinstance(module, nn.Linear) and module.bias is not None:
+        torch.nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.LayerNorm):
+      torch.nn.init.zeros_(module.bias)
+      torch.nn.init.ones_(module.weight)
+    elif isinstance(module, TransformerModel):
+      torch.nn.init.normal_(module.e_pos_emb, mean=0.0, std=0.02)
+      torch.nn.init.normal_(module.d_pos_emb, mean=0.0, std=0.02)
+
+  def forward(self, mfcc, src, tgt=None):
+    # encoder
+    b, et, em = mfcc.size()
+
+    e_position_emb = self.e_position_emb[:, :et, :]
+    
+    ein = self.dense(self.ln(src + e_position_emb))
+
+    # decoder
+    b, dt, v = src.size()
+
+    d_position_emb = self.d_position_emb[:, :dt, :]
+
+    din = self.tok_emb(src) + d_position_emb
+
+    # transformer
+    tgt_mask = self.generate_square_subsequent_mask(dt)
+    out = self.transformer(ein, din, tgt_mask=tgt_mask)
+
+    logits = self.out(out)
+
+    loss = None
+    if tgt is not None:
+      loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+
+    return logits, loss
+
+  def configure_optimizers(self, train_config):
+    
+    decay = set()
+    no_decay = set()
+    whitelist_weight_modules = (torch.nn.Linear, )
+    blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+    for mn, m in self.named_modules():
+      for pn, p in m.named_parameters():
+        fpn = '%s.%s' % (mn, pn) if mn else pn
+
+        if pn.endswith('bias'):
+          no_decay.add(fpn)
+        elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+          decay.add(fpn)
+        elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+          no_decay.add(fpn)
+
+    no_decay.add('e_pos_emb')
+    no_decay.add('d_pos_emb')
+
+    param_dict = {pn: p for pn, p in self.named_parameters()}
+    inter_params = decay & no_decay
+    union_params = decay | no_decay
+    assert len(inter_params) == 0, "parameters into both decay/no_decay sets!"
+    assert len(param_dict.keys() - union_params) == 0, \
+      "parameters not into either decay/no_decay sets!"
+
+    optim_groups = [
+      {"params": [param_dict[pn] for pn in sorted(list(decay))], \
+        "weight_decay": train_config.weight_decay},
+      {"params": [param_dict[pn] for pn in sorted(list(no_decay))], \
+        "weight_decay": 0.0}
+    ]
+
+    optimizer = torch.optim.AdamW(
+      optim_groups, lr=train_config.learning_rate, betas=train_config.betas)
+
+    return optimizer
+
+
+
+
---
+++ AttentionASR/util/set_dataset.py
@@ -0,0 +1,42 @@
+import defect
+import numpy as np
+import librosa
+
+def f(data_dir, tsv_filename):
+  
+  out_tsv_filename = tsv_filename[:-4] + '_mfcc.tsv'
+  
+  with open(data_dir + tsv_filename, 'r') as rf, \
+    open(data_dir + out_tsv_filename, 'w') as wf:
+    
+    for i, line in enumerate(rf):
+      if i == 0:
+        continue
+
+      line = line.split('\t')
+
+      wave_filename = line[1]
+      sentence = line[2]
+      
+      waveform, sr = librosa.load(data_dir + 'clips/' + wave_filename, sr=16000)
+
+      spectrum = defect.sound.logmelspectrogram(waveform, sr)
+
+      np_filename = wave_filename[:-4]
+      np.save(data_dir + 'mfcc/' + np_filename, spectrum)
+
+      token = defect.text.sentence2phoneSymbol(sentence)
+
+      wf.write(data_dir+'mfcc/'+np_filename +'\t' + token)
+
+if __name__ == "__main__":
+  
+  f('./data/cv-corpus/', 'train.tsv')
+  f('./data/cv-corpus/', 'test.tsv')
+
+
+
+    
+    
+
+
